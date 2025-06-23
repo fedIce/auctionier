5d9db3a644a69596bf1d76f484aac467557a9f5e
fedIce <freedez1996@gmail.com>
2025-06-23 15:03:45 +0300

    6/23/25

--- src/jobs/scheduleCloseBidding.ts
+++ src/jobs/scheduleCloseBidding.ts
@@ -1,22 +1,31 @@
 import { TaskHandler } from "payload";
 
-export const topBid = (bids) => {
-  let top = {
+
+
+interface Bid {
+  amount: number;
+  user: string | null;
+  id: string | null;
+}
+
+export const topBid = (bids: Bid[]): Bid => {
+  let top: Bid = {
     amount: 0,
     user: null,
     id: null
-  }
+  };
 
   for (let i = 0; i <= bids.length - 1; i++) {
     if (bids[i].amount > top.amount) {
-      top = bids[i]
+      top = bids[i];
     }
   }
 
-  return top
-}
+  return top;
+};
 
-export const scheduleCloseBidding: TaskHandler<'schedule-close-bidding-task'> = async ({ input, job, req, inlineTask, tasks }) => {
+// export const scheduleCloseBidding: TaskHandler<'schedule-close-bidding-task'> = async ({ input, job, req, inlineTask, tasks }) => {
+export const scheduleCloseBidding = async ({ input, job, req, inlineTask, tasks }: { input: any; job: any; req: { payload: any }; inlineTask: any; tasks: any }) => {
 
   console.log(`Running reminder job  -----------------------------------`);
 
@@ -26,7 +35,8 @@ export const scheduleCloseBidding: TaskHandler<'schedule-close-bidding-task'> = 
     id: input.id
   })
 
-  if (!aucion_item.active && aucion_item.status == 'closed') return { status: false, message: 'auction is closed or deactivated' }
+  if (aucion_item.status == 'closed') return { status: false, message: 'auction is closed' }
+  if (!aucion_item.active) return { status: false, message: 'auction is deactivated' }
 
 
   if (aucion_item.bid_id && typeof aucion_item.bid_id == 'object') {
